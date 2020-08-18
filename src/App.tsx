1c7ee7d2f504a63f80528e0f715b6f1474e96268
Mehul Jariwala <mjariwala98@gmail.com>
2020-08-18 23:20:29 +0530

    tab implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Button } from "reactstrap";
+import { TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap";
 import {
   Dropdown,
   DropdownToggle,
@@ -13,6 +14,11 @@ function App() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [dropdownOpen1, setDropdownOpen1] = useState(false);
   const [dropdownOpen2, setDropdownOpen2] = useState(false);
+  const [activeTab, setActiveTab] = useState("1");
+
+  const tabSwitch = (tab: string) => {
+    if (activeTab !== tab) setActiveTab(tab);
+  };
 
   const toggle = () => setDropdownOpen((prevState) => !prevState);
   const toggle1 = () => setDropdownOpen1((prevState) => !prevState);
@@ -98,9 +104,40 @@ function App() {
 
         <Col></Col>
       </Row>
-      <Row className="root_container-table">
-        <Table />
-      </Row>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={activeTab === "1" ? "active" : ""}
+            onClick={() => {
+              tabSwitch("1");
+            }}
+          >
+            Tab1
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={activeTab === "2" ? "active" : ""}
+            onClick={() => {
+              tabSwitch("2");
+            }}
+          >
+            More Tabs
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId="1">
+          <Row className="root_container-table">
+            <Table />
+          </Row>
+        </TabPane>
+        <TabPane tabId="2">
+          <Row>
+            <Col sm="12">TAB 2</Col>
+          </Row>
+        </TabPane>
+      </TabContent>
     </Container>
   );
 }
