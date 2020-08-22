bf8365eed7424e1e11f2f571fc3f9e7c930d9a14
Mehul Jariwala <30499320+mehuljariwala@users.noreply.github.com>
2020-08-22 08:53:50 +0530

    Create drop2.tsx

---
+++ src/drop2.tsx
@@ -0,0 +1,5 @@
+const accountNumbers = [
+      { label: "INDIA", tab_account_details: "01234",tab_account_sum:"12" },
+      { label: "US", tab_account_details: "01234",tab_account_sum:"12" },
+      { label: "TX", tab_account_details: "01234",tab_account_sum:"12" },
+    ];
